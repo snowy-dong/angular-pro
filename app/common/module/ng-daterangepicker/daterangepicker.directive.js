74b2b2f20f655d1bcd555c4c91eeab5b392c0e60
snowy-dong <99186652@qq.com>
2018-04-24 14:41:12 +0800

    'update'

--- app/common/module/ng-daterangepicker/daterangepicker.directive.js
+++ app/common/module/ng-daterangepicker/daterangepicker.directive.js
@@ -31,6 +31,33 @@ export default function daterangepicker() {
     _init(opts)
     _setEvents()
 
+    function _init(opts) {
+      console.log(opts)
+      if (!opts.startDate) {
+        if (stashDatePicker) delete stashDatePicker.startDate
+        delete opts.startDate
+      }
+      if (!opts.endDate) {
+        if (stashDatePicker) delete stashDatePicker.endDate
+        delete opts.endDate
+      }
+      if (!opts.startDate || !opts.endDate) {
+        scope.model = null
+      }
+      if (!opts.singleDatePicker) {
+        _setDouble(opts)
+      } else {
+        _setSingle(opts)
+      }
+      stashDatePicker = elem.data('daterangepicker');
+      elem.daterangepicker(opts, function (start, end, label) {
+        if (start) scope.dateOptions.startDate = `${start.format(opts.locale.format)}`
+        if (end) scope.dateOptions.endDate = `${end.format(opts.locale.format)}`
+        scope.model = opts.singleDatePicker ? `${start.format(opts.locale.format)}` : `${start.format(opts.locale.format)} ${opts.locale.separator} ${end.format(opts.locale.format)} `
+        scope.$apply(scope.model)
+      });
+    }
+
     function _setSingle(opts) {
       if (Date.parse(opts.startDate) < Date.parse(opts.minDate)) {
         opts.startDate = opts.minDate
@@ -62,32 +89,6 @@ export default function daterangepicker() {
       }
     }
 
-    function _init(opts) {
-      console.log(opts)
-      if (!opts.startDate) {
-        if (stashDatePicker) delete stashDatePicker.startDate
-        delete opts.startDate
-      }
-      if (!opts.endDate) {
-        if (stashDatePicker) delete stashDatePicker.endDate
-        delete opts.endDate
-      }
-      if (!opts.startDate || !opts.endDate) {
-        scope.model = null
-      }
-      if (!opts.singleDatePicker) {
-        _setDouble(opts)
-      } else {
-        _setSingle(opts)
-      }
-      stashDatePicker = elem.data('daterangepicker');
-      elem.daterangepicker(opts, function (start, end, label) {
-        if (start) scope.dateOptions.startDate = `${start.format(opts.locale.format)}`
-        if (end) scope.dateOptions.endDate = `${end.format(opts.locale.format)}`
-        scope.model = opts.singleDatePicker ? `${start.format(opts.locale.format)}` : `${start.format(opts.locale.format)} ${opts.locale.separator} ${end.format(opts.locale.format)} `
-        scope.$apply(scope.model)
-      });
-    }
     // watch 
     scope.$watch(function () {
       return scope.model;
@@ -113,19 +114,6 @@ export default function daterangepicker() {
     }, function (n, o) {
       _init(opts)
     }, true);
-
-    function _setEvents() {
-      const events = ['apply', 'cancel', 'hide', 'showCalendar', 'hideCalendar'];
-      events.forEach(function (eventName) {
-        const localEventName = `date${eventName[0].toUpperCase() + eventName.slice(1)}`;
-        if (angular.isFunction(scope[localEventName])) {
-          elem.on(eventName + '.daterangepicker', e => scope[localEventName]({
-            event: e,
-            ele: elem
-          }))
-        }
-      });
-    }
 
     function _isMoment(d, fmtL) {
       return (!moment(d, opts.locale.format, true).isValid() && d.length >= fmtL)
@@ -153,5 +141,20 @@ export default function daterangepicker() {
         }
       }
     }
+
+    function _setEvents() {
+      const events = ['apply', 'cancel', 'hide', 'showCalendar', 'hideCalendar'];
+      events.forEach(function (eventName) {
+        const localEventName = `date${eventName[0].toUpperCase() + eventName.slice(1)}`;
+        if (angular.isFunction(scope[localEventName])) {
+          elem.on(eventName + '.daterangepicker', e => scope[localEventName]({
+            event: e,
+            ele: elem
+          }))
+        }
+      });
+    }
+
+
   }
 }
